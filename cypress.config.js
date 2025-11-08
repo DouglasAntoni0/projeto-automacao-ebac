2aa45737548cb2a0a2929c7e700f1334502f949b
Douglas Antonio <douglas.tony1235@gmail.com>
2025-11-08 17:08:35 -0300

    Versão inicial do projeto de automação

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "f3zvp5",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
